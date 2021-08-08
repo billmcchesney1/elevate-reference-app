e1d8fabdcf8bcb45c5e9cad63a6c594194769bb8
e050132 <ajit.chauhan@mastercard.com>
2021-08-09 00:04:28 +0530

    Deleted Spring boot specific classes & files.

--- src/test/java/com/mastercard/developers/elevate/accelerator/controller/ServiceEndpoints.java
+++
@@ -1,8 +0,0 @@
-package com.mastercard.developers.elevate.accelerator.controller;
-
-public class ServiceEndpoints {
-	
-	public static final String REDEMTIONS = "/elevate/redemptions";
-	public static final String ELIGIBILITY = "/elevate/eligibilities";
-
-}
